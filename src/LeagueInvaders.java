e39a4a8959aa65ec96a4401b5e29e5155ad42875
league <league@imac-34.attlocal.net>
2022-03-26 14:32:21 -0700

    Coding123123123123123

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
+	JFrame frame = new JFrame();
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 500;
+	GamePanel gpanel = new GamePanel();
 
+	LeagueInvaders() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.pack();
+		
+	}
+
+	void setup() {
+		frame.add(gpanel); 
+		frame.setSize(WIDTH, HEIGHT);
+		
+
+	}
 }
---
+++ src/LeagueInvadersrunner.java
@@ -0,0 +1,10 @@
+
+public class LeagueInvadersrunner {
+public static void main(String[] args) {
+	LeagueInvaders league = new LeagueInvaders(); 
+	league.setup();
+	
+	
+	
+}
+}
